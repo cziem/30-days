bee52690a4007a4e4c04b95baa9812a670096577
Fayvor George <phavorsparks@gmail.com>
2019-04-04 19:58:28 +0100

    add destroyer

---
+++ algorithm/FCC_Alg/destroyer.js
@@ -0,0 +1,16 @@
+function destroyer(arr) {
+  // Remove all the values
+  const args = arguments[0];
+
+  const restArgs = arguments[]
+
+  console.log(restArgs);
+
+  // args.forEach(item => {});
+
+  // return args;
+
+  // return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
