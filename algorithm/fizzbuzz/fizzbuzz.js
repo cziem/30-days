5c79e61e27a8dd0dc1a03aa9c72515aefb806d71
Fayvor George <phavorsparks@gmail.com>
2019-05-13 10:16:49 +0100

    try to work out spinal case

--- algorithm/fizzbuzz/fizzbuzz.js
+++ algorithm/fizzbuzz/fizzbuzz.js
@@ -1,33 +1,7 @@
-/* Using a For loop */
 function fizzBuzz(num) {
-  if (num <= 0) return false;
+	if (num <= 1) return 1;
 
-  let i;
-
-  for (i = 1; i <= num; i++) {
-    if (i % 15 === 0) console.log("FizzBuzz");
-    else if (i % 5 === 0) console.log("Fizz");
-    else if (i % 3 === 0) console.log("Buzz");
-    else console.log(i);
-  }
+	if (num % 5 === 0 && num % 3 === 0) return 'fizzBuzz';
 }
 
-fizzBuzz(20);
-
-/* Uisng a while loop */
-function fizzBuzz(num) {
-  if (num <= 0) return false;
-
-  let i = 0;
-
-  while (i <= num) {
-    if (i % 15 === 0) console.log("FizzBuzz");
-    else if (i % 5 === 0) console.log("Fizz");
-    else if (i % 3 === 0) console.log("Buzz");
-    else console.log(i);
-
-    i++;
-  }
-}
-
-fizzBuzz(20);
+console.log(fizzBuzz(20));
